eaa2e04f8e9cf9dbf8f97e10c20e197b35555e1d
leee549 <1193106371@qq.com>
2020-11-13 00:36:05 +0800

    商品信息增加线程池配置,使用异步编排

---
+++ mall-product/src/main/java/cn/lhx/mall/product/config/MyThreadConfig.java
@@ -0,0 +1,8 @@
+package cn.lhx.mall.product.config;
+
+/**
+ * @author lee549
+ * @date 2020/11/12 15:05
+ */
+public class MyThreadConfig {
+}
---
+++ mall-product/src/main/java/cn/lhx/mall/product/vo/AttrValueWithSkuIdVo.java
@@ -0,0 +1,8 @@
+package cn.lhx.mall.product.vo;
+
+/**
+ * @author lee549
+ * @date 2020/11/12 8:21
+ */
+public class AttrValueWithSkuIdVo {
+}
